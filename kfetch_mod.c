40c44842bac3a60256b9aca79d59fd7481c7162c
Giovani BS <106974254+GolfBravoSierra@users.noreply.github.com>
2025-06-07 17:05:59 -0300

    Update kfetch_mod.c
    Arrumando a Logo

--- kfetch_mod.c
+++ kfetch_mod.c
@@ -73,16 +73,16 @@ static void get_system_info(struct kfetch_data *data, char *buf, size_t size) {
     int pos = 0;
     int logo_idx = 0;
     const char *logo[] = {
-        "   ____ _____ ",
-        "  / ___|___  |",
-        " | |  _   / / ",
-        " | |_| | / /  ",
-        "  \\____|/_/   ",
-        "              ",
-        "              ",
-        "              ",
-        "              ",
-        "              "
+        "  _________________  ",
+        " /  _____/\______  \ ",
+        "/   \  ___    /    / ",
+        "\    \_\  \  /    /  ",
+        " \______  / /____/   ",
+        "       \/            ",
+        "                     ",
+        "                     ",
+        "                     ",
+        "                     "
     };
     const int logo_lines = 6;  // Número real de linhas do logo
     
